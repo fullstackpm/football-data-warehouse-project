02ff6e0e3d88661aa5016e4e50b038e4f1eb0004
sAI <133282289+fullstackpm@users.noreply.github.com>
2026-05-06 23:44:49 +0100

    Create load_bronze.sh

---
+++ scripts/bronze/load_bronze.sh
@@ -0,0 +1,54 @@
+
+
+#!/bin/bash
+
+# Football Data Warehouse - Bronze Layer Loader
+# Usage: ./load_bronze.sh
+
+DB_NAME="FootballDataWarehouse"
+DB_USER="postgres"
+DATA_PATH="/Users/saidursikder/Documents/projects/football-dwh/data"
+
+echo "Loading bronze layer..."
+
+psql -U $DB_USER -d $DB_NAME <<EOF
+
+TRUNCATE TABLE bronze.players_details;
+\copy bronze.players_details FROM '$DATA_PATH/players/players.csv' WITH (FORMAT CSV, HEADER true, DELIMITER ',');
+
+TRUNCATE TABLE bronze.players_valuations;
+\copy bronze.players_valuations FROM '$DATA_PATH/players/player_valuations.csv' WITH (FORMAT CSV, HEADER true, DELIMITER ',');
+
+TRUNCATE TABLE bronze.players_transfers;
+\copy bronze.players_transfers FROM '$DATA_PATH/players/transfers.csv' WITH (FORMAT CSV, HEADER true, DELIMITER ',');
+
+TRUNCATE TABLE bronze.games_appearances;
+\copy bronze.games_appearances FROM '$DATA_PATH/games/appearances.csv' WITH (FORMAT CSV, HEADER true, DELIMITER ',');
+
+TRUNCATE TABLE bronze.games_details;
+\copy bronze.games_details FROM '$DATA_PATH/games/games.csv' WITH (FORMAT CSV, HEADER true, DELIMITER ',');
+
+TRUNCATE TABLE bronze.games_lineups;
+\copy bronze.games_lineups FROM '$DATA_PATH/games/game_lineups.csv' WITH (FORMAT CSV, HEADER true, DELIMITER ',');
+
+TRUNCATE TABLE bronze.games_events;
+\copy bronze.games_events FROM '$DATA_PATH/games/game_events.csv' WITH (FORMAT CSV, HEADER true, DELIMITER ',');
+
+TRUNCATE TABLE bronze.games_club_games;
+\copy bronze.games_club_games FROM '$DATA_PATH/games/club_games.csv' WITH (FORMAT CSV, HEADER true, DELIMITER ',');
+
+TRUNCATE TABLE bronze.competitions_clubs;
+\copy bronze.competitions_clubs FROM '$DATA_PATH/competitions/clubs.csv' WITH (FORMAT CSV, HEADER true, DELIMITER ',');
+
+TRUNCATE TABLE bronze.competitions_details;
+\copy bronze.competitions_details FROM '$DATA_PATH/competitions/competitions.csv' WITH (FORMAT CSV, HEADER true, DELIMITER ',');
+
+TRUNCATE TABLE bronze.competitions_countries;
+\copy bronze.competitions_countries FROM '$DATA_PATH/competitions/countries.csv' WITH (FORMAT CSV, HEADER true, DELIMITER ',');
+
+TRUNCATE TABLE bronze.competitions_national_teams;
+\copy bronze.competitions_national_teams FROM '$DATA_PATH/competitions/national_teams.csv' WITH (FORMAT CSV, HEADER true, DELIMITER ',');
+
+EOF
+
+echo "Done!"
